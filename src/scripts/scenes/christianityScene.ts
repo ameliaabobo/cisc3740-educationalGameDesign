eac30b9b41c4ea7d68b2e854bd589699fec59c3e
Amelia Abobo <aabobo@udel.edu>
2020-05-18 08:32:14 -0400

    adding images

--- src/scripts/scenes/christianityScene.ts
+++ src/scripts/scenes/christianityScene.ts
@@ -1,5 +1,11 @@
 export default class christianityScene extends Phaser.Scene {
     private background: any;
+    private book: any;
+    private food: any;
+    private leader: any;
+    private money: any;
+    private place: any;
+    private vehicle: any;
 
     constructor() {
       super({ key: 'christianityScene' });
@@ -9,6 +15,24 @@ export default class christianityScene extends Phaser.Scene {
         this.background = this.add.image(0, 0, "christianityBackground");
         this.background.setScale(0.73);
         this.background.setOrigin(0,0);
+
+        this.book = this.add.image(100, 100, "christianityBook");
+        this.book.setScale(0.1);
+
+        this.food = this.add.image(200, 200, "christianityFood");
+        this.food.setScale(0.1);
+
+        this.leader = this.add.image(300, 300, "christianityLeader");
+        this.leader.setScale(0.1);
+
+        this.money = this.add.image(400, 400, "christianityMoney");
+        this.money.setScale(0.1);
+
+        this.place = this.add.image(500, 500, "christianityPlace");
+        this.place.setScale(0.1);
+
+        this.vehicle = this.add.image(600, 600, "christianityVehicle");
+        this.vehicle.setScale(0.1);
         
         this.add.text(20, 20, "Christianity Scene", {
           font: "25px Arial",
--- src/scripts/scenes/judaismScene.ts
+++ src/scripts/scenes/judaismScene.ts
@@ -1,5 +1,11 @@
 export default class judaismScene extends Phaser.Scene {
     private background: any;
+    private book: any;
+    private food: any;
+    private leader: any;
+    private money: any;
+    private place: any;
+    private vehicle: any;
 
     constructor() {
       super({ key: 'judaismScene' });
@@ -9,6 +15,25 @@ export default class judaismScene extends Phaser.Scene {
         this.background = this.add.image(250, 0, "judaismBackground");
         this.background.setScale(1.4);
         this.background.setOrigin(0,0);
+
+        this.book = this.add.image(100, 100, "judaismBook");
+        this.book.setScale(0.1);
+
+        this.food = this.add.image(200, 200, "judaismFood");
+        this.food.setScale(0.1);
+
+        this.leader = this.add.image(300, 300, "judaismLeader");
+        this.leader.setScale(0.1);
+
+        this.money = this.add.image(400, 400, "judaismMoney");
+        this.money.setScale(0.1);
+
+        this.place = this.add.image(500, 500, "judaismPlace");
+        this.place.setScale(0.1);
+
+        this.vehicle = this.add.image(600, 600, "judaismVehicle");
+        this.vehicle.setScale(0.1);
+
         
         this.add.text(20, 20, "Judaism Scene", {
           font: "25px Arial",
